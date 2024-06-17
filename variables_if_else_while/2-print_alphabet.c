55dade8d14e9e6283dd92f88f595f7b35298ab5f
Basco <martinez.xabi@laposte.net>
2024-06-17 10:28:23 +0000

    print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ * Return: The alphabet
+*/
+
+int main(void)
+{
+	for(i = 97; i <=122; i++)
+	{
+		putchar(i);
+	}
+        return (0);
+}
