e947678d05c688ccdaecaf5aa5dfd25e4f99f32c
Basco <martinez.xabi@laposte.net>
2024-06-17 13:18:51 +0000

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - a program that prints _putchar, followed by a new line.
+ * Return: "_putchar"
+*/
+
+int main(void)
+{
+	printf("%s\n", _putchar);
+	return (0);
+}
