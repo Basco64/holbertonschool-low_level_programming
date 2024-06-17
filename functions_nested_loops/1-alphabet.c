cc918ed1b024d04a87584131a64561499175504f
Basco <martinez.xabi@laposte.net>
2024-06-17 19:04:55 +0000

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main -  Display alphabet with de main header
+ * Return: alphabet in lower case
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+
+	return (0);
+}
