ffaf9ff9fd1dcec0f98720de5e9085236af30416
Basco <martinez.xabi@laposte.net>
2024-06-17 09:48:05 +0000

    1-last_digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	ld = n%10;
-	printf("Last digit of %d is %d and", n , ld);
+	printf("Last digit of %d is %d and ", n , ld);
 	
 	if (ld > 5)
 		printf("is greater than 5\n");
