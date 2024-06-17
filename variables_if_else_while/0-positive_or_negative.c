f8a53d886ba980bada73d212b051aa2212fb90cf
Basco <martinez.xabi@laposte.net>
2024-06-17 09:56:55 +0000

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+
+/**
+ * main - Prints if the rand num
+ * is positive or negative
+ */
+
 int main(void)
 {
 	int n;
