83b60ca210d6f544a54c5ce0c24104fd59457846
Basco <martinez.xabi@laposte.net>
2024-06-17 11:08:42 +0000

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ * Return: The alphabet
+*/
+
+int main(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
