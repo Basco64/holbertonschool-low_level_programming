34b5cad278bf5db8acd762c0450864b4018cf496
Basco <martinez.xabi@laposte.net>
2024-06-17 11:01:16 +0000

    print alphabet without q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ * Return: The alphabet
+*/
+
+int main(void)
+{
+        int i;
+
+        for (i = 97; i <= 122; i++)
+        {
+		if (i != 113 && i != 101)
+                putchar(i);
+        }
+        putchar('\n');
+        return (0);
+}
