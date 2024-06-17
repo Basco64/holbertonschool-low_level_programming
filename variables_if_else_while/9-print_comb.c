00392ef448337d134493ca48d5f52300ef61e480
Basco <martinez.xabi@laposte.net>
2024-06-17 12:48:49 +0000

    print comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - A program that prints all possible
+ *combinations of single-digit numbers
+ *Return: All single digit
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
