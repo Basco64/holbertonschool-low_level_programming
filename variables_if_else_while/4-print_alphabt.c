18290c43585c6288a14e7ac37db0ed6f2e4505c8
Basco <martinez.xabi@laposte.net>
2024-06-17 11:03:52 +0000

    print alphabet without q and e

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,13 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        for (i = 97; i <= 122; i++)
-        {
+	for (i = 97; i <= 122; i++)
+	{
 		if (i != 113 && i != 101)
-                putchar(i);
-        }
-        putchar('\n');
-        return (0);
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
