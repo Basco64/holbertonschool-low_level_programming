fb91f6411bafada6701311b1e55a66d495ed3da3
Basco <martinez.xabi@laposte.net>
2024-06-17 08:55:12 +0000

    printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+ * main - Prints " with proper grammar, but the outcome is a piece of art,"
+ * 	, followed by a new line.
  *
  * Return: Always 0.
  */
 
 int main(void)
 {
-	printf("%s","with proper grammar, but the outcome is a piece of art,\n");
+	printf("%s","with proper grammar, but the outcome is a piece of art, \n");
 	return 0;
 }
