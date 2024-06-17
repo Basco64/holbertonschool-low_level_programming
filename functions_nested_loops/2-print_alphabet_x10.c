1a0484e05f086ac4984d691d33367db81e601847
Basco <martinez.xabi@laposte.net>
2024-06-17 19:18:50 +0000

    aplhabet *10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print the alphabet .. 10 times
+ * Return: 10 alphabets
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+}
