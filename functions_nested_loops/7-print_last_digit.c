a737b06a090817c89aced23e83228d80a064bc99
Basco <martinez.xabi@laposte.net>
2024-06-18 08:14:32 +0000

    print last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,15 +10,10 @@
 
 int print_last_digit(int num)
 {
-	int last_digit;
+	int last_digit = num % 10;
 
 	if (num < 0)
-	{
 		last_digit *= -1;
-		last_digit = last_digit % 10;
-	}
-	else
-		last_digit = last_digit % 10;
 
 	_putchar(last_digit + '0');
 	return (last_digit);
