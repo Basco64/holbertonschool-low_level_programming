bedbdcc292ac1eb6182a63b7f45de726bd32700b
Basco <martinez.xabi@laposte.net>
2024-06-18 07:20:20 +0000

    is alpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - Check if input is an alpha
+ *
+ * @c: the input to check
+ *
+ * Return: 1 if input is alphabetic character or 0 if is not
+*/
+
+int _isalpha(int c)
+{
+	if (c > 64 && c < 91 || c > 96 && c < 123)
+		return (1)
+	else
+		return (0);
+}
