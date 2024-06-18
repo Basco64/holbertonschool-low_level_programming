53c1f8da8629878fd0edad9205bb1b1e469cfc6f
Basco <martinez.xabi@laposte.net>
2024-06-18 08:01:37 +0000

    print last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function fo print the last digit of a number
+ *
+ * @num: the input
+ *
+ * Return: the last digit of the input
+*/
+
+int print_last_digit(int num)
+{
+	last_digit = num % 10;
+
+	return (last_digit);
+}
