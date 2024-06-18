f794f481924850c33c86640b211a0bcfb6e8c980
Basco <martinez.xabi@laposte.net>
2024-06-18 12:27:28 +0000

    print comb 3

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
-* main -  A program that prints all possible different combinations of two digits
+* main -  A program that prints all possible
+* different combinations of two digits
 *
 * Return: The diffenrents combinations
 */
