c42f46c02710aaba9d093dafeb289702c7ac090a
Basco <martinez.xabi@laposte.net>
2024-06-18 11:01:36 +0000

    quote

---
+++ hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - C program that prints exactly
+ * `and that piece of art is useful" - Dora Korpar, 2015-10-19`
+ * , followed by a new line, to the standard error.
+ *
+ * Return: The print
+*/
+
+int main ()
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1)
+}
