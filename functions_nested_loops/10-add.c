9cc97a04b75cc2b1b56d9068afc48dc730e97568
Basco <martinez.xabi@laposte.net>
2024-06-18 09:10:54 +0000

    simple addition

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Function that adds two integers
+ *
+ * @a: First integer
+ *
+ * @b: Second integer
+ *
+ * Return: The result of additions
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
