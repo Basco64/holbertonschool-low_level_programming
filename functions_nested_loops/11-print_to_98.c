b99df691b3342dcd668d25962483c91a40263eae
Basco <martinez.xabi@laposte.net>
2024-06-18 09:46:19 +0000

    print to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,12 +13,17 @@ void print_to_98(int n)
 {
 	int i;
 
-	if(n == 98)
+	if (n == 98)
 		printf("%d", n);
-	else
+	else if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
 		printf("%d, ", i);
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98 i--)
+			printf("%d, ", i);
 	}
 	_putchar('\n');
 
