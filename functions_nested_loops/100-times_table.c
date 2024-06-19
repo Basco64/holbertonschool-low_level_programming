5128a51b7d9a696cf8655cf4aa0c3ff543e4b9a9
Basco <martinez.xabi@laposte.net>
2024-06-19 06:15:14 +0000

    times tables

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -41,21 +41,12 @@ void print_times_table(int n)
 				_putchar((result % 10) + '0');
 			}
 
-			else if (result == 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar('1');
-				_putchar('0');
-				_putchar('0');
-			}
-
-			else if (result > 100)
+			else if (result >= 100)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar((result / 100) + '0');
-				_putchar((result / 10) + '0');
+				_putchar(((result / 10) % 10) + '0');
 				_putchar((result % 10) + '0');
 			}
 		}
