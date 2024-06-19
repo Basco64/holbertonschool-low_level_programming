ac76ab20a83e8d01b52297fd387a8be45b3b7c73
Basco <martinez.xabi@laposte.net>
2024-06-19 13:24:26 +0000

    104

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -13,12 +13,12 @@ int main(void)
 	 * once you exceed the value of an unsigned long,
 	 * the return is no longer correct. long long forbidden
 	 */
-	int count = 3;
 	unsigned long first = 1, second = 2;
 	unsigned long next = first + second;
+	int count = 3;
 
 	printf("%lu, %lu, ", first, second);
-	while (count <= 98)
+	while (count < 99)
 	{
 		if (count == 98)
 			printf("%lu\n", next);
