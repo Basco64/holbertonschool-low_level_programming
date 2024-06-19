bf65be08e1e3e087e1e3fea3b8bce02ffccc1506
Basco <martinez.xabi@laposte.net>
2024-06-19 07:11:02 +0000

    fibonacci

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -8,18 +8,18 @@
 
 int main(void)
 {
-	long int first = 1, second = 2;
+	long int result = 0, first = 1, second = 2;
 	long int next = first + second;
 
 	while (next < 4000001)
 	{
 		if (next % 2 == 0)
-			printf("%lu, ", next);
+			result += next;
 
 		first = second;
 		second = next;
 		next = first + second;
 	}
-	printf("%lu\n", second);
+	printf("%lu\n", result + second);
 	return (0);
 }
