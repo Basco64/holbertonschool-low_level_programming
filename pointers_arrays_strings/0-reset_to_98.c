b7507bc1c92ffd099ffb959a75305df28278e651
Basco <martinez.xabi@laposte.net>
2024-06-24 13:18:52 +0000

    reset to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Prints the alphabet in lowercase
+ *
+ * @*n: pointer to n
+ *
+ * Return: The reset of 'n'
+*/
+
+void reset_to_98(int *n)
+{
+	&n = 98;
+}
