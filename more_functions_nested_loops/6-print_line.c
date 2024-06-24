2a33ec7a44e3d90415145979c8e5d7d1986da5ae
Basco <martinez.xabi@laposte.net>
2024-06-24 09:08:48 +0000

    print line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line in the terminal
+ *
+ * @n: the input
+ *
+ * Return: the straight line
+*/
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		for (i = 0 ; i < n; i++)
+			putchar('_');
+	}
+	_putchar('\n');
+}
