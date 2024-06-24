ea49cd7ab35cab2ff23f62516c9232fb08392e39
Basco <martinez.xabi@laposte.net>
2024-06-24 08:23:30 +0000

    print most numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints 0 1 3 5 6 7 8 9
+ *
+ * Return: The good numbers
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+		
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+	
+	_putchar('\n');
+}
