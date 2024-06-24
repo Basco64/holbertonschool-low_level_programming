894918520c7572e0a1323c559e287658ea624c4c
Basco <martinez.xabi@laposte.net>
2024-06-24 10:38:32 +0000

    FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz test
+ * Return: void
+*/
+
+void main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if(i % 3 == 0 && i % 5 == 0)
+			printf("%s", "FizzBuzz");
+		else if (i % 3 == 0)
+			printf("%s", "Fizz");
+		else if (i % 5 == 0)
+			printf("%s", "Buzz");
+		else
+			printf("%d", i);
+
+		putchar(' ');
+	}
+	putchar('\n');
+}
