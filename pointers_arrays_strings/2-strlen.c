8b6b7d1b8b94a95c19e98ffa5f770e664f21e6f4
Basco <martinez.xabi@laposte.net>
2024-06-24 13:32:31 +0000

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Return the length of a string
+ *
+ * @s: pointer of a string
+ *
+ * Return: The length of the string
+*/
+
+int _strlen(char *s)
+{
+	int inpLen;
+
+	while ()
+	{}
+
+	return (inpLen);
+}
