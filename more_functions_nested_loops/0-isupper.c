c54da5ccc5c075129377184f35933f0197683a13
Basco <martinez.xabi@laposte.net>
2024-06-24 07:51:28 +0000

    new week

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * @c: the input
+ *
+ * Return: 1 if 'c' is uppercase or 0
+*/
+
+int _isupper(int c)
+{
+	if (c <= 65 && c >= 90)
+		return (1);
+	else
+		return (0);
+}
