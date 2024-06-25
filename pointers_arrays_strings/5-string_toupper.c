bb5f558854050f083178cd29a930ffac68944520
Basco <martinez.xabi@laposte.net>
2024-06-25 18:19:49 +0000

    string to upper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - Switch lowercase to uppercase
+ *
+ *@str: The input string
+ *
+ * Return: The string with uppercase
+*/
+
+char *string_toupper(char *str)
+{
+	int i, len = 0;
+	
+	while (str[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] += 17
+		}
+	}
+}
