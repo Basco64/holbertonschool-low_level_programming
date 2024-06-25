43810710e3e9affa8c80fe9851e3837e48a8b186
Basco <martinez.xabi@laposte.net>
2024-06-25 08:04:29 +0000

    rev string v2

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -16,10 +16,10 @@ void rev_string(char *s)
 	while (s[len] != '\0')
 		len++;
 
-	for (i = len; i > len / 2; i--)
+	for (i = len - 1; i >= len / 2; i--)
 	{
 		tmp = s[i];
-		s[i] = s[len - i];
-		s[len - i] = tmp;
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
 	}
 }
