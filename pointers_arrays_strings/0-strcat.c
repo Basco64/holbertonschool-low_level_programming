6009dffb0ee4ed155082b21ba46bdc72e715cd34
Basco <martinez.xabi@laposte.net>
2024-06-25 13:19:22 +0000

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,16 +11,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int len1 = 0,len2 = 0, i;
-	
+	int len1 = 0, len2 = 0, i;
+
 	while (dest[len1] != '\0')
 		len1++;
 	while (src[len2] != '\0')
 		len2++;
-	
+
 	for (i = 0; i < len2; i++)
 		dest[len1 + i] = src[i];
 	dest[len1 + len2] = '\0';
 
-	return dest;
+	return (dest);
 }
