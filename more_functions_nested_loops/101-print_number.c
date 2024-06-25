a501099f2392a6c93133c0e1c201052b7285bdfd
Basco <martinez.xabi@laposte.net>
2024-06-25 06:18:58 +0000

    print number

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+
+#include "main.h"
+
+/**
+ * print_number -  Prints an integer
+ *
+ * @n: the integer
+ *
+ * Return: the integer
+*/
+
+void print_number(int n)
+{
+	int u, d;
+
+	if (n < 0)
+		_putchar('-');
+
+	d = 100000
+		
+	while (i > 0)
+	{
+		if (i <= u || i == 1)
+			_putchar(u / i % 10 + '0');
+		i /= 10;
+	}
+}
