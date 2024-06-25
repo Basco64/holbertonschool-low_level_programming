2f55c25b38d91a8b60791dc5ced4140208fbe2ef
Basco <martinez.xabi@laposte.net>
2024-06-25 14:54:16 +0000

    reverse array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverses the content of an array of integers
+ *
+ * @a: An array
+ * @n: Number of element in 'a'
+ *
+ *Return: The reverse Array
+ */
+
+void reverse_array(int *a, int n)
+{}
