e2639dfc06b93c325147f35dac8d95683debe784
Basco <martinez.xabi@laposte.net>
2024-06-25 07:53:53 +0000

    rev string v2

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,10 +11,15 @@
 void rev_string(char *s)
 {
 	int len = 0, i;
+	char tmp;
 
 	while (s[len] != '\0')
 		len++;
 
-	for (i = len - 1; i >= 0; i--)
-		_putchar(s[i]);
+	for (i = len - 1; i > len / 2; i--)
+	{
+		tmp = s[i];
+		s[i] = s[len - i];
+		s[len - i] = tmp;
+	}
 }
