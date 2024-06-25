166ab25c40ee22e510ec45fe47d8c0a8d9a7cb17
Basco <martinez.xabi@laposte.net>
2024-06-25 12:31:38 +0000

    keygen

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,3 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+/**
+ * main - Generate a password
+ *
+ * Return: The password
+ */
+
+int main(void)
+{
+	int password[100], i, sum = 0, n;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		password[i] = rand() % 94 + 33;
+		sum += password[i];
+		putchar(password[i]);
+		if (sum >= 2772)
+			break;
+	}
+
+	if (sum != 2772)
+	{
+		n = 2772 - sum;
+		sum += n;
+		putchar(n);
+	}
+
+	return (0);
+}
