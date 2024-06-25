79286c24d010f0e5ecb172e789b1df02020ceac8
Basco <martinez.xabi@laposte.net>
2024-06-25 07:12:49 +0000

    strlen

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Print a string
+ *
+ * @str: pointer of a string
+ *
+ * Return: The string
+*/
+
+int _puts(char *str)
+{
+        int i;
+
+	for (i = 0; i <= _strlen(str); i++)
+		_putchar(str[i]);
+	_putchar('\n');
+	return (0);
+}
