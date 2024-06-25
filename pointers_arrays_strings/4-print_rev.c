72e5ccc0efb8909d1b95364fd1c41c61f46457bd
Basco <martinez.xabi@laposte.net>
2024-06-25 07:21:52 +0000

    print rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_rev - Return the reverse string
+ *
+ * @s: pointer of a string
+ *
+ * Return: The reverse string
+*/
+
+void print_rev(char *s)
+{
+	int len, i;
+
+	while (s[len] != '\0')
+		len ++;
+
+	for (i = len; i <= 0, i--)
+		_putchar(s[i]);
+	_putchar('\n');
+
+}
