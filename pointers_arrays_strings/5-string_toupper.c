8aeb84afcbbf0ad1255c5112341fa148e48caa5b
Basco <martinez.xabi@laposte.net>
2024-06-26 07:21:40 +0000

    string to upper

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -11,14 +11,14 @@
 char *string_toupper(char *str)
 {
 	int i, len = 0;
-	
+
 	while (str[len] != '\0')
 		len++;
 
 	for (i = 0; i < len; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] += 'A' - 'a';
+			str[i] -= 32;
 	}
 	return (str);
 }
