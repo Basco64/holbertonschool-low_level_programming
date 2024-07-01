d1901b9782f150a122bc5dfcbd3039e4b29189a9
Basco <martinez.xabi@laposte.net>
2024-07-01 12:57:41 +0000

    print rev

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev_recursion - Return the reverse string
+ *
+ * @s: pointer of a string
+ *
+ * Return: The reverse string
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
