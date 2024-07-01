e99da8599b8d4c967f86d233c9393a31adc2bb9a
Basco <martinez.xabi@laposte.net>
2024-07-01 07:29:05 +0000

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * strchr - Function that locates a character in a string
+ *
+ * @s: The string
+ * @c: The character
+ *
+ * Return: A pointer to the character or NULL
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+
+	return (NULL);
+}
