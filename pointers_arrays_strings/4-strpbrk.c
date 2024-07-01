f021bc9e950b84b57b2d7afaa44489cc1a71b2ae
Basco <martinez.xabi@laposte.net>
2024-07-01 09:21:40 +0000

    strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Function that searches a string for any of a set of bytes
+ *
+ * @s: The string
+ * @accept: The bytes accepted
+ *
+ * Return: The pointer to the match byte 
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+
+	return (NULL);
+}
