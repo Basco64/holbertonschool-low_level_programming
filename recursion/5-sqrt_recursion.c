088d63e74973eeb24aaaddd8b658f601b660cc41
Basco <martinez.xabi@laposte.net>
2024-07-01 16:46:49 +0000

    test si 2 fonctions ca passe

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+int find_sqrt(int n, int sqrt) {
+	if (sqrt * sqrt == n)
+		return sqrt;
+	if (sqrt * sqrt > n)
+		return -1;
+	return find_sqrt(n, guess + 1);
+}
+
 /**
  * _sqrt_recursion - Function that returns the natural square root of a number
  *
@@ -9,4 +17,9 @@
  */
 
 int _sqrt_recursion(int n)
-{}
+{
+	if (n < 0)
+		return -1;
+	
+	return find_sqrt(n, 0);
+}
