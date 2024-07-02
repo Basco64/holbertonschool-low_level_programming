eb5a2073413a8bf8cd960c8a3a04d746e47c26f0
Basco <martinez.xabi@laposte.net>
2024-07-02 13:33:49 +0000

    start change

---
+++ argc_argv/100-change.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the minimum number of
+ * coins to make change for an amount of money
+ *
+ * @argc: Number of argument in 'argv'
+ * @argv: Arguments Values (Array of String)
+ *
+ * Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+
+}
