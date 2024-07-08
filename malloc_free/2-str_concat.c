2d674d8234ecc3272e98aef2c3579b06c7993399
Basco <martinez.xabi@laposte.net>
2024-07-08 08:56:59 +0000

    str concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,4 +15,31 @@
 char *str_concat(char *s1, char *s2)
 {
 	int len1, len2, lenT, i;
+	char *newStr;
+
+	len1 = len2 = 0;
+
+	while (s1[len1])
+		len1++;
+
+	while (s2[len2])
+		len2++;
+
+	lenT = len1 + len2;
+
+	newStr = malloc((lenT + 1) * sizeof(char));
+
+	for (i = 0; i < len1; i++)
+	{
+		newStr[i] = s1[i];
+	}
+
+	for (; i < lenT; i++)
+	{
+		newStr[i] = s2[i - len1];
+	}
+
+	newStr[i] = '\0';
+
+	return (newStr);
 }
