aa20a8c73aa06b8c61f76d228ce05202774c0af9
Basco <martinez.xabi@laposte.net>
2024-07-08 18:51:50 +0000

    argstostr init

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * argstostr - Function that concatenates all the arguments of your program
+ *
+ * @ac: Number of argument in 'av'
+ * @av: Arguments Values (Array of String)
+ *
+ * Return: the args one line at a time
+ */
+
+char *argstostr(int ac, char **av)
+{
+}
