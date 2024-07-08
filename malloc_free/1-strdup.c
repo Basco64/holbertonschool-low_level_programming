8c4a67a933c6a39dcd08071799a439456fd1a28a
Basco <martinez.xabi@laposte.net>
2024-07-08 08:21:07 +0000

    strdup

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -4,7 +4,7 @@
 #include "main.h"
 
 /**
- * _strup - Function that returns a pointer to a newly
+ * _strdup - Function that returns a pointer to a newly
  * allocated space in memory, which contains
  * a copy of the string given as a parameter
  *
