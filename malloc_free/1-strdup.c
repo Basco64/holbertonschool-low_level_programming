5a3701bb614fefc95c267493a6af0cadd7308abe
Basco <martinez.xabi@laposte.net>
2024-07-08 08:14:20 +0000

    strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * _strup - Function that returns a pointer to a newly
+ * allocated space in memory, which contains
+ * a copy of the string given as a parameter
+ *
+ * @str: The source string
+ *
+ * Return: Pointer to the new allocated memory
+ */
+
+char *_strdup(char *str)
+{
+	int i, len;
+	char *newStr;
+
+	if (str == NULL)
+		return (NULL);
+	
+	while (str[len])
+		len++;
+	len++;
+
+	newStr = malloc(len * sizeof(char));
+
+	for (i = 0; i < len; i++)
+	{
+		newStr[i]=str[i];
+	}
+
+	return (newStr);
+}
