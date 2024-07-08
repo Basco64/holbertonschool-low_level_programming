3c969e24240f34d9997b93c823eab44d919f25fc
Basco <martinez.xabi@laposte.net>
2024-07-08 18:28:06 +0000

    alloc grid

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -25,9 +25,9 @@ int **alloc_grid(int width, int height)
 	if (grid == NULL)
 		return (NULL);
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(height * sizeof(int));
+		grid[i] = malloc(width * sizeof(int));
 
 		if (grid[i] == NULL)
 		{
