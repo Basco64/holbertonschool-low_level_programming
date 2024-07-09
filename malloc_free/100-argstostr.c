bc6dca5868444d2950242a56091c3c33caaadbcb
Basco <martinez.xabi@laposte.net>
2024-07-09 07:38:40 +0000

    argstostr

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -9,9 +9,39 @@
  * @ac: Number of argument in 'av'
  * @av: Arguments Values (Array of String)
  *
- * Return: the args one line at a time
+ * Return: A pointer to a new string, or NULL if it fails
  */
 
 char *argstostr(int ac, char **av)
 {
+	char *arg;
+	int i, j, k, length = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			length++;
+		length++;
+	}
+	arg = malloc((length * sizeof(char)) + 1);
+
+	if (arg == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			arg[k] = av[i][j];
+			k++;
+		}
+		arg[k] = '\n';
+		k++;
+
+	}
+	arg[k] = '\0';
+	return (arg);
 }
