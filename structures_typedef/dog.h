1ae7f0f7158e423486a814ec09b05c4192c85f0a
Basco <martinez.xabi@laposte.net>
2024-07-09 10:33:04 +0000

    Time to lunch

---
+++ structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog struct
+ *
+ * @name: Name
+ * @age: Age
+ * @owner: Owner
+ *
+ * Description: A simple struct of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/**
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+*/
+
+#endif
