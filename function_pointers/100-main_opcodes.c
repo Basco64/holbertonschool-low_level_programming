4a3cf8260e55d25a2bc9e3cf8e0b55bf3feb00c0
Basco <martinez.xabi@laposte.net>
2024-07-15 09:55:52 +0000

    test opcode

---
+++ function_pointers/100-main_opcodes.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Function that prints the opcodes of its own main function
+ *
+ * @argc: Argument count
+ * @argv: Arguments Values (Array of String)
+ *
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int arg = atoi(argv[1]);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (arg < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < arg; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+
+		if (arg > i)
+			printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
