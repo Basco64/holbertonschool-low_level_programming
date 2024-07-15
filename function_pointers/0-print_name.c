31a47bfe802185c6b87913f354bf958d0f3f45b2
Basco <martinez.xabi@laposte.net>
2024-07-15 07:32:07 +0000

    start function pointers

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - Function that prints a name
+ *
+ * @name: The name to print
+ * @f: Function who take a string in argument
+ *
+ * Return: Void
+*/
+
+void main(int argc, char **argv)
+{
+	f(name);
+}
