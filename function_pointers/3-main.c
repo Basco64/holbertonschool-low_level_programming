e47e9e11928f26e25a423ede500c871b5972919a
Basco <martinez.xabi@laposte.net>
2024-07-18 13:10:53 +0000

    like that

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 	operator = get_op_func(argv[2]);
 	b = atoi(argv[3]);
 
-	if (operator == NULL)
+	if (operator == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
