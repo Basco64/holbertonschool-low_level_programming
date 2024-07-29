7e75be0fe751544aa6e4f9629fea25a13be7804c
Basco <martinez.xabi@laposte.net>
2024-07-29 13:11:31 +0000

    add beginning -> doubly

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,13 +1,24 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Function that prints all the elements of a dlistint_t list
+ * add_dnodeint - Function that adds a new node at
+ * the beginning of a dlistint_t list
  *
  * @h: Header
  *
- * Return: The number of nodes
+ * Return: The adress of the new element
  */
 
-size_t print_dlistint(const dlistint_t *h)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *newElem = malloc(size(dlistint_t));
+	
+	if (newElem == NULL)
+		return (NULL);
+
+	newElem->n = n;
+	newElem->prev = NULL;
+	newElem-next = head;
+
+	return (newElem);
 }
