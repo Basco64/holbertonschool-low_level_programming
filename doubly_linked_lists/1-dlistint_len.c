11c31ffbff1c54f44b5fa9657553f664b29443d9
Basco <martinez.xabi@laposte.net>
2024-07-29 13:03:30 +0000

    doubly -> len

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,14 +1,15 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Function that prints all the elements of a dlistint_t list
+ * dlistint_len - Function that returns the number of
+ * elements in a linked dlistint_t list
  *
  * @h: Header
  *
  * Return: The number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t len = 0;
 	const dlistint_t *current = h;
---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Function that prints all the elements of a dlistint_t list
+ *
+ * @h: Header
+ *
+ * Return: The number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+}
