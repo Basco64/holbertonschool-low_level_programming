b2239133a96eb59994a432cbe2f62a48c9e5eed3
Basco <martinez.xabi@laposte.net>
2024-07-29 19:18:39 +0000

    get nth node in double linked list

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- *get_dnodeint_at_index - Function that returns the nth node of a dlistint_t linked list
+ *get_dnodeint_at_index - Function that returns the nth node
+ of a dlistint_t linked list
  *
  * @head: Pointer to head of the linked list
  * @index: Index of the node
