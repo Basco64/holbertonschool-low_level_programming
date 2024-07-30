c1ab8944a07ec8a911f8ef6367a3759504d8bcf5
Basco <martinez.xabi@laposte.net>
2024-07-30 19:52:35 +0000

    Finish task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,6 +13,8 @@
 int main(int argc, char **argv)
 {
 	ssize_t from, to;
+	char buffer[1024];
+	int from_source, to_dest;
 
 
 	if (argc != 3)
@@ -35,15 +37,33 @@ int main(int argc, char **argv)
 		exit(99);
 	}
 
+	from_source = 1024;
+	while (from_source == 1024)
+	{
+		from_source = read(from, buffer, 1024);
+		if (from_source == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+
+		to_dest = write(to, buffer, from_source);
+		if (to_dest == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
 	if (close(from) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %s\n", argv[1]);
+		dprintf(2, "Error: Can't close fd %ld\n", from);
 		exit(100);
 	}
 
 	if (close(to) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %s\n", argv[2]);
+		dprintf(2, "Error: Can't close fd %ld\n", to);
 		exit(100);
 	}
 
