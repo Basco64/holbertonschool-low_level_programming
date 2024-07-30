a9e0dc8bddd70651b906b29c35369b87ad9feb0b
Basco <martinez.xabi@laposte.net>
2024-07-30 13:41:24 +0000

    Add test for task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -38,7 +38,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	buffer[readed] = '\0';
 
-	printed = write(1, buffer, readed);
+	if (readed > 0)
+		printed = write(1, buffer, readed);
 
 	free(buffer);
 	close(file);
