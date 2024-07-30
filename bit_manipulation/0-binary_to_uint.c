0c7b2ae5bb3b4da565f05f6e7822db6a05292c58
Basco <martinez.xabi@laposte.net>
2024-07-30 08:22:41 +0000

    Binary to uint

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,7 +11,21 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res;
+	unsigned int res = 0, i;
 
+	if (b == NULL)
+		return (0);
 
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		res = res << 1;
+
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		if (b[i] == '1')
+			res = res | 1;
+	}
+
+	return (res);
 }
