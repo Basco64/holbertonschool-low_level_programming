b7c0e841b6a19f4d69c1dfb6f02dd37758c565a1
Basco <martinez.xabi@laposte.net>
2024-07-30 14:17:31 +0000

    Add task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -4,11 +4,38 @@
  * append_text_to_file - Function that appends text at
  * the end of a file
  *
- * @filename: Path to the file
+ * @filename: The filename
  * @text_content: The content
  *
- * Return: The real number of letters
+ * Return: 1 on success and -1 on failure
  */
 
 int append_text_to_file(const char *filename, char *text_content)
-{}
+{
+	int file, printed, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(file);
+		return (1);
+	}
+
+	while (text_content[len] != '\0')
+		len++;
+
+	printed = write(file, text_content, len);
+
+	close(file);
+
+	if (printed == -1 || printed != len)
+		return (-1);
+
+	return (1);
+}
