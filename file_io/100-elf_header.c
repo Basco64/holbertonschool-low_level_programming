ad1fdbe88501dae1b30d9b3117dce5d0741ab00c
Basco <martinez.xabi@laposte.net>
2024-07-31 12:29:01 +0000

    Change header Task 4

--- file_io/100-elf_header.c
+++ file_io/100-elf_header.c
@@ -1,4 +1,11 @@
-#include "main.h"
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * An ELF (Executable and Linkable Format) file is a standard file
  * format for executable files, object code, shared libraries,
